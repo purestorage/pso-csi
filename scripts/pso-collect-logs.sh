4a70820d9b326ee9a07236165ac740bc820d91ea
“HaibinMichaelXie” <hxie@purestorage.com>
2021-02-09 22:13:03 +0000

    make kubectl as variable for openshift to use oc, get pso namespace automatically

--- scripts/pso-collect-logs.sh
+++ scripts/pso-collect-logs.sh
@@ -1,16 +1,23 @@
-
-if [[ $# != 1 ]]; then
-  echo -e "If kubeconfig is not configured please run: \nexport KUBECONFIG=[kube-config-file]\n"
-  echo -e "Usage: *.sh [PSO-Namespace]"
+if [[ $# != 0 ]]; then
+  echo -e "If kubeconfig is not configured please run: export KUBECONFIG=[kube-config-file]\n"
+  echo -e "For openshift cluster, please change KUBECTL variable to oc\n"
+  echo -e "Usage: *.sh"
   exit
 fi
 
-PSO_NS=$1
+# Please change to oc for openshift cluster.
+KUBECTL=kubectl
 LOG_DIR=./pso-logs
 
-DB_REPLICAS=$(kubectl get statefulset -n $PSO_NS | awk '{print $1}' | grep pso-db-)
+PSO_NS=$($KUBECTL get pod -o wide --all-namespaces | grep pso-csi-controller-0 | awk '{print $1}')
 
-PODS=$(kubectl get pod -n $PSO_NS | awk '{print $1}' | grep -e pso-db- -e pso-csi-)
+tput setaf 2;
+echo -e "PSO namespace is $PSO_NS, log dir is $LOG_DIR\n"
+tput sgr0
+
+DB_REPLICAS=$($KUBECTL get statefulset -n $PSO_NS | awk '{print $1}' | grep pso-db-)
+
+PODS=$($KUBECTL get pod -n $PSO_NS | awk '{print $1}' | grep -e pso-db- -e pso-csi-)
 
 rm $LOG_DIR -r
 mkdir $LOG_DIR
@@ -21,43 +28,44 @@ do
 
   # Get log of volume-publish container and cockroachdb container for pso-db-*-0 pods.
   if [[ $pod == "pso-db-"*"-0" ]]; then
-    kubectl logs $pod -c volume-publish -n $PSO_NS > $LOG_DIR/$pod-volume-publish.log
-    kubectl logs $pod -c cockroachdb -n $PSO_NS > $LOG_DIR/$pod-cockroachdb.log
+    $KUBECTL logs $pod -c volume-publish -n $PSO_NS > $LOG_DIR/$pod-volume-publish.log
+    $KUBECTL logs $pod -c cockroachdb -n $PSO_NS > $LOG_DIR/$pod-cockroachdb.log
   fi
 
   if [[ $pod == *"cockroach-operator"* ]]; then
-    kubectl logs $pod -c cockroach-operator -n $PSO_NS > $LOG_DIR/$pod.log
+    $KUBECTL logs $pod -c cockroach-operator -n $PSO_NS > $LOG_DIR/$pod.log
   fi
 
   if [[ $pod == *"db-deployer"* ]]; then
-    kubectl logs $pod -c db-deployer -n $PSO_NS > $LOG_DIR/$pod.log
+    $KUBECTL logs $pod -c db-deployer -n $PSO_NS > $LOG_DIR/$pod.log
   fi
 
   if [[ $pod == *"pso-csi-"* ]]; then
-    kubectl logs $pod -c pso-csi-container -n $PSO_NS > $LOG_DIR/$pod.log
+    $KUBECTL logs $pod -c pso-csi-container -n $PSO_NS > $LOG_DIR/$pod.log
   fi
 done
 
 echo "collect info for all pods"
-kubectl get pod --all-namespaces -o wide > $LOG_DIR/all-pods.log
+$KUBECTL get pod --all-namespaces -o wide > $LOG_DIR/all-pods.log
 echo -e "\n" >> $LOG_DIR/all-pods.log
-kubectl describe pod -n $PSO_NS >> $LOG_DIR/all-pods.log
+$KUBECTL describe pod -n $PSO_NS >> $LOG_DIR/all-pods.log
 
 echo "collect logs for all nodes"
-kubectl get node -o wide > $LOG_DIR/all-nodes.log
+$KUBECTL get node -o wide > $LOG_DIR/all-nodes.log
 
 echo "collect logs for all pvcs"
-kubectl get pvc -o wide > $LOG_DIR/all-pvcs.log
+$KUBECTL get pvc -o wide > $LOG_DIR/all-pvcs.log
 echo -e "\n" >> $LOG_DIR/all-pvcs.log
-kubectl describe pvc >> $LOG_DIR/all-pvcs.log
+$KUBECTL describe pvc >> $LOG_DIR/all-pvcs.log
 
 echo "collect logs for all pvs"
-kubectl get pv -o wide > $LOG_DIR/all-pvs.log
+$KUBECTL get pv -o wide > $LOG_DIR/all-pvs.log
 echo -e "\n" >> $LOG_DIR/all-pvs.log
-kubectl describe pv >> $LOG_DIR/all-pvs.log
+$KUBECTL describe pv >> $LOG_DIR/all-pvs.log
 
 echo "collect logs for all resources in PSO namespace"
-kubectl get all -o wide -n $PSO_NS > $LOG_DIR/all-resource.log
+$KUBECTL get all -o wide -n $PSO_NS > $LOG_DIR/all-resource.log
 echo -e "\n" >> $LOG_DIR/all-resource.log
-kubectl describe all -n $PSO_NS >> $LOG_DIR/all-resource.log
+# Supress potential error: Error from server (NotFound): the server could not find the requested resource
+$KUBECTL describe all -n $PSO_NS >> $LOG_DIR/all-resource.log 2>/dev/null
 
